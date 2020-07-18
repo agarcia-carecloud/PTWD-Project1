fee5fde1892e5d484851627c77072142707edbd7
Alejandro Garcia <agarc371@gmail.com>
2020-07-18 09:31:05 -0400

    code cleanup

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const gettingStarted = document.getElementById("getting-started");
 const startBtn = document.getElementById('start-btn');
-const playAgain = document.getElementById('play-again');
+// const playAgain = document.getElementById('play-again');
 // playAgain.style.display = 'none';
 
 startBtn.addEventListener('click', (event) => { //load game once start button is clicked
